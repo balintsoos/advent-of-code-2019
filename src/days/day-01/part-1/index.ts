518993a2608d1f8760b92ad0416d5047330b0374
Balint Soos <soosbalint95@gmail.com>
2019-12-01 19:03:14 +0100

    refactor(days): day 1 part 1

--- src/days/day-01/part-1/index.ts
+++ src/days/day-01/part-1/index.ts
@@ -1,11 +1,13 @@
 import { inputObservable } from '../../../libs/input-observable';
 import { join } from 'path';
-import { reduce } from 'rxjs/operators';
+import { map, reduce } from 'rxjs/operators';
 
 export const calculateFuel = (mass: number): number => Math.floor(mass / 3) - 2;
-export const accumulateFuels = (acc: number, mass: number): number => acc + calculateFuel(mass);
+export const add = (acc: number, value: number): number => acc + value;
+export const sum = reduce(add, 0);
 
 export default () =>
   inputObservable(join(__dirname, '../input.txt'))
-    .pipe(reduce(accumulateFuels, 0))
+    .pipe(map(calculateFuel))
+    .pipe(sum)
     .subscribe((fuel: number) => console.log(fuel));
