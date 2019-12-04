89f811d8672fc6d4d75249b2d0e93f8ecae81efa
Balint Soos <soosbalint95@gmail.com>
2019-12-04 02:08:12 +0100

    refactor(days): day 3 part 1

--- src/days/day-03/part-1/index.ts
+++ src/days/day-03/part-1/index.ts
@@ -7,57 +7,53 @@ export type Coordinate = {
   posY: number;
 };
 
+const ORIGO: Coordinate = { posX: 0, posY: 0 };
+
 export const readLines = (filePath: string): string[][] => {
   const file = readFileSync(filePath, { encoding: 'utf8' });
   const lines = file.split('\n').map(line => line.split(','));
   return [lines[0], lines[1]];
 };
 
-export const buildCoords = (instructions: string[]): Coordinate[] => {
+export const newCoord = (currentCoord: Coordinate, direction: string): Coordinate => {
+  switch (direction) {
+    case 'U':
+      return { posX: currentCoord.posX, posY: currentCoord.posY + 1 };
+
+    case 'D':
+      return { posX: currentCoord.posX, posY: currentCoord.posY - 1 };
+
+    case 'L':
+      return { posX: currentCoord.posX + 1, posY: currentCoord.posY };
+
+    case 'R':
+      return { posX: currentCoord.posX - 1, posY: currentCoord.posY };
+
+    default:
+      return currentCoord;
+  }
+};
+
+export const buildCoords = (instructions: string[], origo: Coordinate): Coordinate[] => {
   const coords: Coordinate[] = [];
   instructions.forEach((instruction: string) => {
     const direction = instruction[0];
     const stepCount = parseInt(instruction[1]);
-    const currentCoord = last(coords) ?? { posX: 0, posY: 0 };
-    switch (direction) {
-      case 'U':
-        for (let step = 0; step < stepCount; step++) {
-          coords.push({ posX: currentCoord.posX, posY: currentCoord.posY + 1 });
-        }
-        break;
-
-      case 'D':
-        for (let step = 0; step < stepCount; step++) {
-          coords.push({ posX: currentCoord.posX, posY: currentCoord.posY - 1 });
-        }
-        break;
-
-      case 'L':
-        for (let step = 0; step < stepCount; step++) {
-          coords.push({ posX: currentCoord.posX - 1, posY: currentCoord.posY });
-        }
-        break;
-
-      case 'R':
-        for (let step = 0; step < stepCount; step++) {
-          coords.push({ posX: currentCoord.posX + 1, posY: currentCoord.posY });
-        }
-        break;
-
-      default:
-        break;
+    for (let step = 0; step < stepCount; step++) {
+      const currentCord = last(coords) ?? origo;
+      coords.push(newCoord(currentCord, direction));
     }
   });
   return coords;
 };
 
-export const distanceFromOrigo = (coord: Coordinate): number => {
-  return Math.abs(coord.posX) + Math.abs(coord.posY);
+export const distance = (coordA: Coordinate, coordB: Coordinate): number => {
+  return Math.abs(coordA.posX - coordB.posX) + Math.abs(coordA.posY - coordB.posY);
 };
 
 export const processLines = (lines: string[][]): number => {
-  return intersection(buildCoords(lines[0]), buildCoords(lines[1]))
-    .map(distanceFromOrigo)
+  return intersection(buildCoords(lines[0], ORIGO), buildCoords(lines[1], ORIGO))
+    .map(intersection => distance(ORIGO, intersection))
     .reduce(min);
 };
 
